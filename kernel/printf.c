b03e28394c380bde304cbeebb16f3b8b467a2f95
engine <2878341748@qq.com>
2023-05-19 03:04:00 -0700

    ready to submit my lab

--- kernel/printf.c
+++ kernel/printf.c
@@ -133,3 +133,17 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void
+backtrace(void)
+{
+  printf("backtrace:\n");
+  // fp保存当前栈帧指针
+  uint64 fp=r_fp();
+  while(fp!=PGROUNDUP(fp))
+  {
+    uint64 ret_addr=*(uint64 *)(fp-8);
+    printf("%p\n", ret_addr);
+    fp=*(uint64 *)(fp-16);
+  }
+}
