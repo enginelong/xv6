cc6f4ffbb98ee8b67cb44c4263b0bcfd05bed5a7
engine <2878341748@qq.com>
2023-05-19 20:21:24 -0700

    ready to submit my lab

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,6 +63,10 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+void            add_page_ref(void *);
+void            dec_page_ref(void *);
+int             get_page_ref(void *);
+
 
 // log.c
 void            initlog(int, struct superblock*);
@@ -147,6 +151,7 @@ void            trapinit(void);
 void            trapinithart(void);
 extern struct spinlock tickslock;
 void            usertrapret(void);
+int             cowcheck(pagetable_t,uint64);
 
 // uart.c
 void            uartinit(void);
