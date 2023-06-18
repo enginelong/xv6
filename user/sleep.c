e559db394b6afeb54a32359c927a826a754f93b1
enginelong <2878341748@qq.com>
2023-06-18 06:48:17 -0700

    ready to submit my lab

--- user/sleep.c
+++ user/sleep.c
@@ -2,12 +2,12 @@
 #include "user/user.h"
 
 int main(int argn, char *argv[]){
-	if(argn != 2){
-		fprintf(2, "must 1 argument for sleep\n");
-		exit(1);
-	}
-	int sleepNum = atoi(argv[1]);
-	printf("(nothing happens for a little while)\n");
-	sleep(sleepNum);
-	exit(0);
+    if(argn != 2){
+	fprintf(2, "must 1 argument for sleep\n");
+	exit(1);
+    }
+    int sleepNum = atoi(argv[1]);
+    printf("(nothing happens for a little while)\n");
+    sleep(sleepNum);
+    exit(0);
 }
